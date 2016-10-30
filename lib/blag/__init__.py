2846a3854a14adeb5662b44678476aa0b09ec68b
Ryan Rion <charlesh2016@usd402.com>
2016-10-30 13:30:10 -0700

    lib.blag.util: use new decorator to print out routes and methods

--- lib/blag/__init__.py
+++ lib/blag/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from tinydb import TinyDB
 
+
 app = Flask(__name__)
 db = TinyDB('posts.json')
 config = {}
+
+
+def add_route(route, methods):
+    print(route, repr(methods))
+    return app.route(route, methods=methods)
--- lib/blag/rest.py
+++ lib/blag/rest.py
@@ -1,27 +1,20 @@
 from flask import jsonify, abort, request
-from . import app  # @app.route()
-from . import util
+from . import util, add_route, app  # noqa F401 // @add_route()
 # util.get_post_list() util.get_post() util.add_post(), util.update_post()
 # util.delete_post()
 
 
-print('/api/v1/posts GET')
-
-
-@app.route('/api/v1/posts', methods=['GET'])
+@add_route('/api/v1/posts', methods=['GET'])
 def get_post_list():
     return jsonify([post for post in util.get_post_list()])
     # ::TODO:: yieldify util.get_post_list()
 
 
-print('/api/v1/post/<int:eid> GET')
-
-
-@app.route('/api/v1/post/<int:eid>', methods=['GET'])
+@add_route('/api/v1/post/<int:eid>', methods=['GET'])
 def get_post(eid):
     post_to_send = None
     for post in util.get_post_list():
-        if post == None:
+        if post is None:
             break
         elif post["eid"] == eid:
             post_to_send = post
@@ -32,25 +25,16 @@ def get_post(eid):
         return jsonify(post_to_send)
 
 
-print('/api/v1/new POST')
-
-
-@app.route('/api/v1/new', methods=['POST'])
+@add_route('/api/v1/new', methods=['POST'])
 def make_post():
     return jsonify({"eid": util.add_post(request)})
 
 
-print('/api/v1/posts/<int:eid> PUT/POST')
-
-
-@app.route('/api/v1/posts/<int:eid>', methods=['PUT', 'POST'])  # don't POST
+@add_route('/api/v1/posts/<int:eid>', methods=['PUT', 'POST'])  # don't POST
 def amend_post(eid):
     return jsonify({"eid": util.update_post(eid, request)})
 
 
-print('/api/v1/posts/<int:eid> DELETE ::NOTEST TODO::')
-
-
-@app.route('/api/v1/posts/<int:eid>', methods=['DELETE'])
+@add_route('/api/v1/posts/<int:eid>', methods=['DELETE'])
 def delete_post(eid):
     return util.delete_post(eid)
