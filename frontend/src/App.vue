8a8412142795071361f142796a9eb2bf6549f8c6
Ryan Rion <charlesh2016@usd402.com>
2016-10-31 15:34:07 -0700

    frontend/: start working on cleaning frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <home></home>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+import Home from './components/Home';
 
 export default {
   name: 'app',
   components: {
-    Hello,
+    Home,
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
