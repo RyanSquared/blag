a1f4e2b746f7083b3924c32b03bf2d7889028645
Ryan Rion <charlesh2016@usd402.com>
2016-10-30 23:28:29 -0700

    frontend/config/index.js: add /static/ to assets url

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../../static/index.html'),
     assetsRoot: path.resolve(__dirname, '../../static'),
     assetsSubDirectory: '.',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/static',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
