b22f7a6e0e7a99a7e37f88c48248d130973d2da9
Ryan Rion <charlesh2016@usd402.com>
2016-10-31 20:47:20 -0700

    static/js/app.js: add initial frontend code

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,20 @@
-new Vue({
-	el: '#app',
-	data: {},
-});
-
 $(document).ready(function() {
   setTimeout(function() {
     $('html').fadeIn(500);
   }, 500);
+
+  $('#set-up-editor').click(function() {
+    $('#post-title h1').html('<input type="text" placeholder="Title">');
+    $('#post-body').html('<textarea placeholder="Content">');
+    $('#post-actions').html('<button class="mdl-button mdl-js-button ' +
+        'mdl-js-ripple mdl-button--accent" id="btn-post">Post</button>&nbsp;' +
+        '<button class="mdl-button mdl-js-button mdl-js-ripple ' +
+        'mdl-button--accent" id="btn-discard">Discard</button>');
+  });
+
+  $(document).on('click', '#btn-discard', function() {
+    $('#post-title h1').html('');
+    $('#post-body').html('');
+    $('#post-actions').html('');
+  });
 });
