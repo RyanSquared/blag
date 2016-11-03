3665eab2bb092e63be4a4d4a8935f17403607797
Ryan Rion <charlesh2016@usd402.com>
2016-11-03 12:55:22 -0700

    static/js/app.js: add previous and next button functionality and rearrange post display functions

--- static/js/app.js
+++ static/js/app.js
@@ -40,35 +40,45 @@ $(document).ready(function() {
   // if the server returns 404 - for instane, if a post is not found, either
   // disable the '#next' / '#prev' or show the initial text
 
-  var posts = [], post, bottom_post;
+  var posts = [], post;
 
-  function reload(count) {
+  function showPost(eid) {
+    // pass an EID to be "shifted down" to find a post
+    // you can also pass the direct EID for a post
+    var post_index = eid ? eid : posts.length;
+    var old_post = post;
+    while (post_index >= 0 && !posts[post_index])
+      post_index--;
+
+    if (post = posts[post_index]) {
+      fadeOutAll(()=> {
+        title.html(post.title);
+        body.html(post.post);
+        actions.html(normal_actions);
+        fadeInAll(null, true);
+      });
+    } else if (posts.length == 0) {
+      // do not trigger if you have a post loaded
+      fadeOutAll(()=> {
+        title.html(base_title);
+        body.html(base_body);
+        actions.html(base_actions);
+        fadeInAll();
+      })
+    } else {
+      post = old_post; // restore old post to memory
+    }
+  }
+
+  function reload(start, direction) {
     editing = false;
-    var url = count ? '/api/v1/posts?start_eid=' + count : '/api/v1/posts'
-    $.getJSON(url).then((response_data)=> {
+    var url = start ? '/api/v1/posts' + (direction ? '/reverse' : '') +
+      '?start_eid=' + start : '/api/v1/posts';
+    return $.getJSON(url).then((response_data)=> {
       for (index in response_data) {
         posts[response_data[index].eid] = response_data[index];
       }
 
-      var post_index = posts.length;
-      while (post_index >= 0 && !posts[post_index])
-        post_index--;
-
-      if (post = posts[post_index]) {
-        fadeOutAll(()=> {
-          title.html(post.title);
-          body.html(post.post);
-          actions.html(normal_actions);
-          fadeInAll(null, true);
-        });
-      } else {
-        fadeOutAll(()=> {
-          title.html(base_title);
-          body.html(base_body);
-          actions.html(base_actions);
-          fadeInAll();
-        })
-      }
     });
   }
 
@@ -76,7 +86,7 @@ $(document).ready(function() {
     info_header = $('#info .mdl-card__title').hide();
     info_element = $('#info .mdl-card__supporting-text').html('').hide();
     $('html').fadeIn(500, ()=> {
-      reload();
+      reload().then(()=> showPost());
       $.getJSON('/api/v1/config').then((response)=> {
         data = response.about;
         for (index in data) {
@@ -127,7 +137,7 @@ $(document).ready(function() {
       post_source: $('#post-body textarea').val(),
       post: markdown.toHTML($('#post-body textarea').val()),
     };
-    $.post('/api/v1/new', data).then(()=> reload());
+    $.post('/api/v1/new', data).then(()=> reload().then(()=> showPost()));
   });
 
   $('#delete').on('click', function() {
@@ -135,16 +145,9 @@ $(document).ready(function() {
       url: '/api/v1/posts/' + post.eid,
       type: 'DELETE',
       success: (()=> {
-        var length = posts.length;
-        delete posts[length - 1];
-        do {
-          if (posts[length]) {
-            break;
-          }
-          delete posts[posts.length];
-          length -= 1;
-        } while (length >= 0)
-        reload();
+        var eid = post.eid;
+        delete posts[eid];
+        showPost(eid - 1);
       }),
     });
   });
@@ -167,4 +170,58 @@ $(document).ready(function() {
       '&nbsp;<button class="mdl-button mdl-js-button mdl-js-ripple ' +
       'mdl-button--accent" id="btn-discard">Discard</button>');
   }));
+
+  $(document).on('click', '#old', function() {
+    // check how many are left in buffer
+    var count = 0;
+    var older_post = undefined;
+    for (var current = post.eid - 1; current > 0; current--) {
+      if (posts[current]) {
+        if (!older_post)
+          older_post = posts[current];
+        count++;
+      }
+    }
+    if (count == 0)
+      reload(post.eid).then(()=> showPost(post.eid - 1));
+    else if (count < 2)
+      reload(post.eid).then(()=> showPost(older_post.eid));
+    else if (older_post)
+      showPost(older_post.eid);
+  });
+
+  $(document).on('click', '#new', function() {
+    var count = 0;
+    var newer_post = undefined;
+    for (var current = post.eid + 1; current < posts.length; current++)
+      if (posts[current]) {
+        if (!newer_post)
+          newer_post = posts[current];
+        count++;
+      }
+    if (count < 2)
+      reload(post.eid + 1, true).then(function() {
+        if (newer_post) {
+          return showPost(newer_post.eid);
+        }
+        // iterate up to find post
+        var current; // will use later
+        for (current = post.eid + 1; current < posts.length; current++)
+          if (posts[current])
+            break;
+        if (!posts[current])
+          return;
+        showPost(posts[current].eid);
+      });
+    else {
+      // iterate up to find post
+      var current; // will use later
+      for (current = post.eid + 1; current < posts.length; current++)
+        if (posts[current])
+          break;
+      if (!posts[current])
+        return;
+      showPost(posts[current].eid);
+    }
+  });
 });
