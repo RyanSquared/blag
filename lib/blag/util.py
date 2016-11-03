adf622c6812ea71bd8827b47886ab5c85bfbc546
Ryan Rion <charlesh2016@usd402.com>
2016-11-03 12:54:28 -0700

    lib/blag/util.py: fix SQL queries and add reverse-list

--- lib/blag/util.py
+++ lib/blag/util.py
@@ -7,19 +7,30 @@ def get_post_list(count=10, start=-1):  # generator
     :returns: generator(post, [post...])
     """
     if start == -1:
-        start = db_cursor.execute("SELECT COUNT(*) FROM posts").fetchone()[0]
+        start = db_cursor.execute("SELECT MAX(eid) FROM posts").fetchone()[0]
     if start == 0:
         return  # no posts in database
     for post in db_cursor.execute(
-            """SELECT * FROM posts ORDER BY eid DESC LIMIT ?""", (count, )):
+            """SELECT * FROM posts WHERE eid <= ? ORDER BY eid DESC LIMIT ?""",
+            (start, count)):
+        yield dict(zip(('eid', 'title', 'post', 'post_source'), post))
+
+
+def get_reverse_post_list(count=10, start=0):  # generator
+    """Yield a list of posts, starting from oldest
+
+    :returns: generator(post, [post...])
+    """
+    for post in db_cursor.execute(
+            """SELECT * FROM posts WHERE eid >= ? ORDER BY eid ASC LIMIT ?""",
+            (start, count)):
         yield dict(zip(('eid', 'title', 'post', 'post_source'), post))
 
 
 def get_post(eid):
     return dict(
-        zip(('eid', 'title', 'post'),
-            db_cursor.execute("""SELECT * FROM posts WHERE id = ?""", (eid,
-                                                                       ))))
+        zip(('eid', 'title', 'post'), db_cursor.execute(
+            """SELECT * FROM posts WHERE eid = ?""", (eid,))))
 
 
 def add_post(request):
