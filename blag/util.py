3ea8e5de06c4a34706f67e832c050defe5b6f794
ChickenNuggers <ryan@hashbang.sh>
2016-11-24 00:05:00 -0600

    blag/util.py: invalid request exception class and check_auth(password) function

--- blag/util.py
+++ blag/util.py
@@ -1,6 +1,27 @@
-from . import db_cursor, db
+from . import db_cursor, db, config
+from hashlib import sha512
 
 query = "eid,title,post,post_source".split(',')
+
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+def check_auth(password):
+    return config["password"] == sha512(password).hexdigest()
 
 
 def get_post_list(count=10, start=-1):  # generator
